261da99f6615a1dce0371b8152b5ced3b3c90ebd
colebarclay <barccw22@wfu.edu>
2024-01-31 12:15:00 -0500

    Created.

--- src/FileInputOutput.java
+++ src/FileInputOutput.java
@@ -1,8 +1,35 @@
+import java.io.FileInputStream;
+import java.util.Scanner;
+import java.io.PrintWriter;
+
 public class FileInputOutput {
 
     public static void main(String[] args) {
 //       1) Open the given file inside src called moviesDataSet.csv
+        PrintWriter fileWriter;
+        FileInputStream myFile = null;
+        try {
+            myFile = new FileInputStream("src/moviesDataset.csv");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+
+        Scanner fileReader = new Scanner(myFile);
+        while (fileReader.hasNextLine()) {
+            String data = fileReader.nextLine();
+            String[] arr = data.split(",");
+//            System.out.print(arr[2] + " ");
+//            System.out.println(arr[4]);
+            fileWriter = new PrintWriter(myFile);
+            fileWriter.println(arr[4]);
+        }
+        myFile.close();
+        fileWriter.close();
+
 //       2) Read each row in the dataset
+
+
 //       3) Print each row to the screen
 //       4) Print only the name of the movie
 //       5) Add exception handling (try & catch) when needed
@@ -10,5 +37,4 @@ public class FileInputOutput {
 //       7) Commit your code and push it to the GitHub
 
     }
-
 }
